7b1ef9a155f4246e68f5e903885f6391b5c5f717
Dima Nikolaev <elionsea@mail.ru>
2016-05-13 15:53:27 +0300

    update map;

--- js/leaf-map.js
+++ js/leaf-map.js
@@ -1,16 +1,16 @@
 ;
 // create a map in the "map" div, set the view to a given place and zoom
-var map = L.map('mapid').setView([48.5125571,34.5946928], 20);
+var map = L.map('mapid').setView([48.512, 34.594], 13);
 
 // add an OpenStreetMap tile layer
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    // attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 var myIcon = L.icon({
-    iconUrl: '../img/Google-icon.png',
-    iconRetinaUrl: '../img/Google-icon.png',
-    iconSize: [44, 44],
+    iconUrl: '../img/logo-lada.png',
+    iconRetinaUrl: '../img/logo-lada.png',
+    iconSize: [44, 44]
     // iconAnchor: [22, 94],
     // popupAnchor: [-3, -76],
     // shadowUrl: 'my-icon-shadow.png',
@@ -19,4 +19,4 @@ var myIcon = L.icon({
     // shadowAnchor: [22, 94]
 });
 
-L.marker([48.5125571,34.5946928], {icon: myIcon}).addTo(map);
+L.marker([48.512, 34.594], {icon: myIcon}).addTo(map);
