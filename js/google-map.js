4d0d1e0fe41ebd68924b1d0ea61f595a85447bbe
Dima Nikolaev <elionsea@mail.ru>
2016-05-17 00:29:14 +0300

    fix the map;

--- js/google-map.js
+++ js/google-map.js
@@ -1,4 +1,22 @@
-map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: -34.397, lng: 150.644},
-  zoom: 8
+;
+window.initMap = function () {
+	var map;
+	myOptions = {
+	  center: {lat: 48.5149281, lng: 34.588731},
+	  zoom: 17
+	};
+  map = new google.maps.Map(document.getElementById("map"), myOptions);	  
+  google.maps.visualRefresh = true;
+
+  var marker = new google.maps.Marker({
+    position: {lat: 48.5125316, lng: 34.5968432},
+    map: map,
+    icon: 'img/Google-icon.png'
+  });
+
+};
+
+$(document).on('mousemove', function (e){
+	google.maps.event.trigger(map, 'resize');
 });
+
